7656d62889f4a4ebdc232ec71f3f0fe516114ac5
Gurjit Singh <gurjits081@gmail.com>
2025-05-01 00:38:16 +0530

    chore: update Prisma Client and Query Engine versions; add auth middleware and user registration route

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,55 @@
+import { db } from '../libs/db.js';
+import bcrypt from 'bcryptjs';
+import { UserRole } from '../libs/roles.js';
+import jwt from 'jsonwebtoken';
+
+export const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const existingUser = await db.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (existingUser) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await db.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        role: UserRole.USER,
+      },
+    });
+
+    const token = jwt.sign({id: newUser.id}, process.env.JWT_SECRET, {expiresIn: "7d"});
+
+    const cookieOptions = {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    }
+
+    res.cookie('token', token, cookieOptions);
+
+    res.status(201).json({
+      success: true,
+      message: 'User registered successfully',
+      user: {
+        id: newUser.id,
+        name: newUser.name,
+        email: newUser.email,
+        role: newUser.role,
+      },
+    });
+  } catch (error) {
+    console.error('Error registering user:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
